9534f5972e988851c0062b789138becab2f5d5de
Mustafa88A <mustafa0780500@gmail.com>
2023-07-17 19:52:57 +0300

    it is for loop

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-function calculateFactorial(number) {}
+function calculateFactorial(number) {
+   console.log(number);
+   let factorial = 1;
+ 
+   for (let i=1; i<=number; i++)
+   {factorial=factorial*i;
+   }
+return factorial;
+}
+
+console.log(calculateFactorial(5));
+
+// let result=7;
+// const mustafa = (a,b) => {
+// result = a +b
+// return result
+// }
+// console.log(result)
+// console.log(mustafa(1,2))
