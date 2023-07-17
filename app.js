078990f2761025a080f65af7310e00348e3f82ee
abbas-abdalhusain-yosif <abs_abdal@yahoo.com>
2023-07-17 09:02:57 -0700

    solve Loop-Task

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-function calculateFactorial(number) {}
+function calculateFactorial(number) {
+    let factorial=1;
+    for(let counter=number;counter>0;--counter)
+    {
+        factorial =factorial*counter;
+    }
+    console.log(factorial);
+}
+calculateFactorial(5);
+calculateFactorial(10);
+calculateFactorial(15);
