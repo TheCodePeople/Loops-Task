c26749b384b58bb2f8e80e41c5a8b4cb95bc76cd
sameralmusawiy.sa@gmail.com <sameralmusawiy.sa@gmail.com>
2023-07-17 19:11:27 +0300

    Last one

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-function calculateFactorial(number) {}
+function calculateFactorial(number) {
+    
+  for (let i = number - 1; i >= 1; i--) {
+    number *= i;
+  }
+  console.log(number); 
+
+}
+
+calculateFactorial(5);
+
