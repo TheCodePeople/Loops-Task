c76ec97465c1e4cb8af595741441d4a6301be063
haneenjaleel <haneenjaleel7@gmail.com>
2023-07-17 19:15:07 +0300

    solve loop

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-function calculateFactorial(number) {}
+function calculate (number ){
+let factorial=1 ;
+
+for(let i=1 ; i<=number; i++){
+factorial=factorial*i;
+}
+console.log(factorial)}
+calculate(5);
