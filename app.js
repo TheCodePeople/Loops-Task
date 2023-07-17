2d47c117e23fcc26ee51f04f40d6273d4bd6a24f
3lawi <alii.3mmar@gmail.com>
2023-07-17 18:44:41 +0300

    Complete Task

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-function calculateFactorial(number) {}
+
+
+
+function factorial(num) {
+    let factorial = 1;
+    for (let i = 1; i <= num; i++) {
+        factorial *= i;
+    }
+    console.log(`The factorial of ${num} is ${factorial}.`);
+}
+
+let num = prompt("Enter a number: ");
+factorial(num);
