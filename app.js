34ad34014c4528604f53fc7e05966e0b78b8788c
zainabyahya <zainabh_2015@yahoo.com>
2024-02-14 17:13:52 +0300

    solved the task

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-function calculateFactorial(number) {}
+function calculateFactorial(number) {
+    let factorial = 1;
+    for (let i = 1; i <= number; i++) {
+        factorial *= i;
+    }
+    return factorial;
+}
+
+while (true) {
+    let number = parseInt(prompt("Enter a number:"));
+    alert(`Factorial of ${number} is ${calculateFactorial(number)}`);
+}
