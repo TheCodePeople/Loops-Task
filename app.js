6ed07f638f1b600f0ef35806cc043fe51828f942
Mahdi209 <dr.arcadea@gmail.com>
2024-02-14 17:09:22 +0300

    the task is done

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-function calculateFactorial(number) {}
+function calculateFactorial(number) {
+  let fact = 1;
+  for (let i = 2; i <= number; i++) {
+    fact *= i;
+  }
+  return fact;
+}
+
+let number = 5;
+console.log(calculateFactorial(number));
