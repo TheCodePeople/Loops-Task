c6fa6564d5edc1cde9816eb3a73caa8058258b14
bit-sercererx <ameerahmed202397@gmail.com>
2024-02-14 17:40:56 +0300

    i doin

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-function calculateFactorial(number) {}
+function calculateFactorial(number) {
+    let factorial = 1;
+    for (let i = 1; i <= number; i++) {
+      factorial *= i;
+    }
+    return factorial;
+  }
+  console.log(calculateFactorial(5));
+  
