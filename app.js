b85a0a4305f3151784f851b4d81cdfeb61d3db40
mohamedali0122 <mohamedalhelo909@gmail.com>
2024-02-14 17:33:05 +0300

    done task top

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-function calculateFactorial(number) {}
+function calculateFactorial(number) {
+    let ruslt =1
+    for (let i=1 ; i <= number ; i++){
+         ruslt = ruslt * i;
+        console.log(ruslt);
+    };
+};
+
+calculateFactorial(5);
